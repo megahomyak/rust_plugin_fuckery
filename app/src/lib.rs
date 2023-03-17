0c673889d2132ef56c43326247a9f895fa187dfb
megahomyak <g.megahomyak@gmail.com>
2023-03-17 13:16:55 +0500

    Trying to make it work; it does not

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,5 +1,18 @@
+#[macro_use]
+extern crate dlopen_derive;
+use dlopen::wrapper::{Container, WrapperApi};
+
+#[derive(WrapperApi)]
+struct PluginApi {
+    run: extern "C" fn(),
+}
+
 pub fn run() {
     println!("Starting App");
+
+    let plugin_api_wrapper: Container<PluginApi> =
+        unsafe { Container::load("plugins/libplugin1.so") }.unwrap();
+    plugin_api_wrapper.run();
 }
 
 pub fn test_app_func(message: &str) {

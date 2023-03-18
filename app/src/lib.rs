5f20b73b1b197313b2b7c23162248583c7d5718c
megahomyak <g.megahomyak@gmail.com>
2023-03-18 10:01:50 +0500

    Moved the contents of `run()` to `main()`

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,20 +1,3 @@
-#[macro_use]
-extern crate dlopen_derive;
-use dlopen::wrapper::{Container, WrapperApi};
-
-#[derive(WrapperApi)]
-struct PluginApi {
-    run: extern "C" fn(),
-}
-
-pub fn run() {
-    println!("Starting App");
-
-    let plugin_api_wrapper: Container<PluginApi> =
-        unsafe { Container::load("../plugin1/target/debug/libplugin1.so") }.unwrap();
-    plugin_api_wrapper.run();
-}
-
 pub fn test_app_func(message: &str) {
     println!("test_app_func(\"{}\")", message);
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,16 @@
-use app;
+#[macro_use]
+extern crate dlopen_derive;
+use dlopen::wrapper::{Container, WrapperApi};
+
+#[derive(WrapperApi)]
+struct PluginApi {
+    run: extern "C" fn(),
+}
 
 fn main() {
-    app::run();
+    println!("Starting App");
+
+    let plugin_api_wrapper: Container<PluginApi> =
+        unsafe { Container::load("../plugin1/target/debug/libplugin1.so") }.unwrap();
+    plugin_api_wrapper.run();
 }

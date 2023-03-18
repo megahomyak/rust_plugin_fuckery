5b9a79636e1df53b9da8bb35953af3635d75be23
megahomyak <g.megahomyak@gmail.com>
2023-03-18 11:28:33 +0500

    Tried to make a Plugin trait

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,7 +4,11 @@ use dlopen::wrapper::{Container, WrapperApi};
 
 #[derive(WrapperApi)]
 struct PluginApi {
-    run: extern "C" fn(),
+    get_plugin: extern "C" fn() -> *mut dyn Plugin,
+}
+
+pub trait Plugin {
+    fn handle_command(&self, command: &str);
 }
 
 fn main() {
@@ -12,5 +16,23 @@ fn main() {
 
     let plugin_api_wrapper: Container<PluginApi> =
         unsafe { Container::load("../plugin1/target/debug/libplugin1.so") }.unwrap();
-    plugin_api_wrapper.run();
+    let plugin = unsafe { Box::from_raw(plugin_api_wrapper.get_plugin()) };
+
+    loop {
+        // Prompt
+        println!("Enter command:");
+
+        // Read input
+        let mut command = String::new();
+        std::io::stdin().read_line(&mut command).unwrap();
+
+        // Trim newline
+        command = command.trim().into();
+
+        // Check command
+        if command == "exit" {
+            break;
+        }
+        plugin.handle_command(&command);
+    }
 }
--- plugin1/src/lib.rs
+++ plugin1/src/lib.rs
@@ -1,7 +1,16 @@
 extern crate app;
 
+struct Plugin1;
+
+impl app::Plugin for Plugin1 {
+    fn handle_command(&self, command: &str) {
+        if command.starts_with("say ") {
+            println!("{}", command.chars().skip(4).as_str());
+        }
+    }
+}
+
 #[no_mangle]
-pub fn run() {
-    println!("Running plugin1");
-    app::test_app_func("Hello from plugin 1");
+pub fn get_plugin() -> *mut dyn app::Plugin {
+    Box::into_raw(Box::new(Plugin1))
 }

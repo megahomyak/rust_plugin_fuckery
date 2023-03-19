2f9e4f685a7ad98b5c0cb8fdf4f68fa82ca0f232
megahomyak <g.megahomyak@gmail.com>
2023-03-20 00:07:03 +0500

    Started writing the `Plugin` trait

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,13 @@
 use libloading::{Library, Symbol};
 
+pub trait Plugin: Any + Sync + Send {
+
+}
+
 type AddFunc = unsafe fn(usize, usize) -> usize;
 
 fn main() {
-    for _ in 0..100 {
-        let plugin = unsafe { Library::new("plugins/plugin1.so") }.unwrap();
-        let func: Symbol<AddFunc> = unsafe { plugin.get(b"add") }.unwrap();
-        println!("{}", unsafe { func(1, 2) });
-    }
+    let plugin = unsafe { Library::new("plugins/plugin1.so") }.unwrap();
+    let func: Symbol<AddFunc> = unsafe { plugin.get(b"add") }.unwrap();
+    println!("{}", unsafe { func(1, 2) });
 }

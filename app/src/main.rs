653749a873909890ef174963b39d7d9a1b246002
megahomyak <g.megahomyak@gmail.com>
2023-03-19 21:33:26 +0500

    Tested it for RAM usage, it does not leak

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,7 +3,9 @@ use libloading::{Library, Symbol};
 type AddFunc = unsafe fn(usize, usize) -> usize;
 
 fn main() {
-    let plugin = unsafe { Library::new("plugins/plugin1.so") }.unwrap();
-    let func: Symbol<AddFunc> = unsafe { plugin.get(b"add") }.unwrap();
-    println!("{}", unsafe { func(1, 2) });
+    for _ in 0..100 {
+        let plugin = unsafe { Library::new("plugins/plugin1.so") }.unwrap();
+        let func: Symbol<AddFunc> = unsafe { plugin.get(b"add") }.unwrap();
+        println!("{}", unsafe { func(1, 2) });
+    }
 }
